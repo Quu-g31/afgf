50d6892bf9b0f2b2afbbc883b6cbe90253ce3c23
Quu-g31 <lst-ofzf@docomo.ne.jp>
2021-09-22 15:32:13 +0900

    action cableの導入

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get 'comments/new'
   devise_for :users
   root to: 'posts#index'
-  resources :posts, only: [:new, :create]
+  resources :posts, only: [:new, :create] do
+    resources :comments, only: [:new, :create]
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -35,8 +35,12 @@ ActiveRecord::Schema.define(version: 2021_09_21_090504) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "text"
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -72,5 +76,7 @@ ActiveRecord::Schema.define(version: 2021_09_21_090504) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
